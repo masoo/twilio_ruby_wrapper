9e8a8106e83785d6fd8dccb71693ad34a49c542d
FUNABARA Masao <masao@masoo.jp>
2017-08-30 14:19:50 +0900

    add Call and Queue Class.

--- lib/twilio_ruby_wrapper/queue.rb
+++ lib/twilio_ruby_wrapper/queue.rb
@@ -3,7 +3,14 @@ module TwilioRubyWrapper
     attr_reader :sid, :account_sid, :friendly_name, :uri, :current_size, :average_wait_time, :max_size, :date_created, :date_updated
     attr_accessor :queue_instance
 
+    def self.set_twilio_params(account_sid:, auth_token:)
+      @@account_sid = account_sid
+      @@auth_token = auth_token
+      true
+    end
+
     def initialize(twilio_queue_instance)
+      @twilio_client = Twilio::REST::Client.new(@@account_sid, @@auth_token)
       @queue_instance = twilio_queue_instance
       @sid = @queue_instance.sid
       @account_sid = @queue_instance.account_sid
@@ -17,7 +24,9 @@ module TwilioRubyWrapper
     end
 
     def calls
-      @queue_instance.members.list().map{|twilio_call| Call.new(twilio_call) }
+      @queue_instance.members.list().map do |member|
+        Call.new(@twilio_client.account.calls.get(member.call_sid))
+      end
     end
   end
 end
